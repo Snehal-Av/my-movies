3e88d9d021099a36c0dcf3f60541abf763249864
Snehal-Av <113681654+Snehal-Av@users.noreply.github.com>
2024-05-31 10:13:02 +0530

    Update
    Update

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,9 +3,16 @@ import MovieCard from "./MovieCard";
 import axios from "axios";
 import Pagination from "./Pagination";
 
-const Movies = ({ handleWatchList, handleRemoveFromWatchList, watchList,i }) => {
+const Movies = ({ handleWatchList, handleRemoveFromWatchList, watchList, i }) => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false)
   const [pageNo, setPageNo] = useState(1)
+
+  const postPerPage = 16
+
+  const lastIndex = pageNo * postPerPage
+  const firstIndex = lastIndex - postPerPage
+  const pages = movies.slice(firstIndex, lastIndex)
 
   const prevPage = () => {
     if (pageNo === 1) {
@@ -13,17 +20,25 @@ const Movies = ({ handleWatchList, handleRemoveFromWatchList, watchList,i }) => 
     }
     else {
       setPageNo(pageNo - 1)
+
     }
   }
   const nextPage = () => {
-    setPageNo(pageNo + 1)
+    let page = pageNo + 1
+    setPageNo(page)
+    if (page >= 15) {
+      page = 15
+      setPageNo(page)
+    }
   }
 
 
   useEffect(() => {
+    setLoading(true)
     axios.get(`https://api.tvmaze.com/shows?page=${pageNo}`).then((response) => {
       console.log(response.data)
       setMovies(response.data);
+      setLoading(false)
     }, [pageNo]);
   }, []);
   return (
@@ -31,14 +46,16 @@ const Movies = ({ handleWatchList, handleRemoveFromWatchList, watchList,i }) => 
       <div className="t-movies">Treanding Movies</div>
       <div className="movie-card">
 
-        {movies.map((movieObj) => {
-          return <MovieCard key={movieObj.id} movieObj={movieObj} img={movieObj.image.medium}
-            name={movieObj.name} handleWatchList={handleWatchList}
-            handleRemoveFromWatchList={handleRemoveFromWatchList}
-            watchList={watchList}
-           i={i}
-          />;
-        })}
+        {
+          loading ? <h5>Loading...</h5> :
+            pages.map((movieObj) => {
+              return <MovieCard key={movieObj.id} movieObj={movieObj} img={movieObj.image.medium}
+                name={movieObj.name} handleWatchList={handleWatchList}
+                handleRemoveFromWatchList={handleRemoveFromWatchList}
+                watchList={watchList}
+                i={i}
+              />;
+            })}
 
       </div>
       <div>
